c435590d61e217167c00fb5c71644f0426121eab
mcastroa7 <manucastroa7@gmail.com>
2025-07-24 17:24:59 -0300

    agregando modulos

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,12 +9,14 @@ import { UserService } from "src/user/user.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/user/entity/user.entity";
 import { JwtStrategy } from "./strategies/jwt.strategy";
+import { MailModule } from "src/mail/mail.module";
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User]),
         PassportModule,
         UserModule,
+        MailModule,
         JwtModule.register({
             secret: process.env.JWT_SECRET || "tuSecretoMuySeguro",
             signOptions: { expiresIn: "1h" },
