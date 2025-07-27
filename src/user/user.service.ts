c8f9e01dca708ffb52f38bdb3ce7b475769e1550
mcastroa7 <manucastroa7@gmail.com>
2025-07-27 19:19:07 -0300

    subiendo cambios ultimos

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "./entity/user.entity"; // ajustá ruta si necesario
-import bcrypt from "bcrypt";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UserService {
