50e0aee0a34e08f78c9d1d492dc85ab873175712
mcastroa7 <manucastroa7@gmail.com>
2025-08-01 15:15:01 -0300

    cambiando el banneo

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User } from './entity/user.entity';// ajustá si lo movés
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), MailModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService], // importante para usarlo en auth.module
